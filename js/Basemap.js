9d8636dbb2bbab03b654136a33460a76376f4368
ewindahl <ewindahl@esri.com>
2018-06-08 16:52:29 -0700

    Revert "Revert "Merge branch 'master' of https://github.com/Esri/imagery-viewer-template""
    This reverts commit 45d351d30ba9f551c49dd52dc4f394d6d1ed2e72.

--- js/Basemap.js
+++ js/Basemap.js
@@ -17,107 +17,44 @@
 define([
     'dojo/_base/declare',
     "dojo/Evented",
-    "dojo/dom",
     'esri/dijit/BasemapGallery',
-    'dojo/_base/lang',
-    "dojo/dom-style", "dijit/registry",
-    "dojo/_base/html",
-    "dojo/query"
+    'dojo/_base/lang'
+    
 
 ],
         function (
                 declare, Evented,
-                dom,
                 BasemapGallery,
-                lang, domStyle, registry,
-                html,
-                query
-
-                ) {
+                lang) {
             return declare("application.Basemap", [Evented], {
                 constructor: function (parameters) {
                     var defaults = {
-                        map: null
+                        map: null,
+                        main:null
                     };
                     lang.mixin(this, defaults, parameters);
                 },
                 basemapGallery: null,
                 spatialRef: null,
                 startup: function () {
-                    /*jshint unused: false*/
                     this.inherited(arguments);
-                    this.initBasemaps();
-                   
                 },
-                resize: function () {
-                    //this._responsive();
-                    if (window.innerWidth > 1200) {
-                        domStyle.set(dom.byId("basemapDialog"), "width", "190px");
-                        domStyle.set(dom.byId("basemapDialog"), "height", "300px");
-                        domStyle.set("basemapDialog", "left", "auto");
-                        domStyle.set("basemapDialog", "right", "20px");
-                        domStyle.set("basemapDialog", "top", "210px");
-                    } else if (window.innerWidth > 1000) {
-                        domStyle.set(dom.byId("basemapDialog"), "width", "160px");
-                        domStyle.set(dom.byId("basemapDialog"), "height", "200px");
-                        domStyle.set("basemapDialog", "left", "auto");
-                        domStyle.set("basemapDialog", "right", "20px");
-                        domStyle.set("basemapDialog", "top", "210px");
-                    } else if (window.innerWidth > 800) {
-                        domStyle.set(dom.byId("basemapDialog"), "width", "150px");
-                        domStyle.set(dom.byId("basemapDialog"), "height", "150px");
-                        domStyle.set("basemapDialog", "left", "auto");
-                        domStyle.set("basemapDialog", "right", "20px");
-                        domStyle.set("basemapDialog", "top", "208px");
-                    } else if (window.innerWidth > 600) {
-                        domStyle.set(dom.byId("basemapDialog"), "width", "150px");
-                        domStyle.set(dom.byId("basemapDialog"), "height", "120px");
-                        domStyle.set("basemapDialog", "left", "auto");
-                        domStyle.set("basemapDialog", "right", "20px");
-                        domStyle.set("basemapDialog", "top", "206px");
-                    } else if (window.innerWidth > 400) {
-                        domStyle.set(dom.byId("basemapDialog"), "width", "130px");
-                        domStyle.set(dom.byId("basemapDialog"), "height", "100px");
-                        domStyle.set("basemapDialog", "left", "auto");
-                        domStyle.set("basemapDialog", "right", "20px");
-                        domStyle.set("basemapDialog", "top", "204px");
-                    } else {
-                        domStyle.set(dom.byId("basemapDialog"), "width", "110px");
-                        domStyle.set(dom.byId("basemapDialog"), "height", "90px");
-                        domStyle.set("basemapDialog", "left", "auto");
-                        domStyle.set("basemapDialog", "right", "20px");
-                        domStyle.set("basemapDialog", "top", "204px");
-                    }
-                },
-                _responsive: function () {
-                    var paneNode = dom.byId("basemapDialog");
-                    var width = html.getStyle(paneNode, 'width');
-                    var column = parseInt(width / 105, 10);
-                    if (column > 0) {
-                        var margin = width % 105;
-                        var addWidth = parseInt(margin / column, 10);
-                        query('.esriBasemapGalleryNode', this.id).forEach(function (node) {
-                            html.setStyle(node, 'width', 85 + addWidth + 'px');
-                        });
-                    }
-                },
-                initBasemaps: function () {
+                postCreate: function () {
                     var config = {};
                     config.map = this.map;
                     config.portalUrl = "http://www.arcgis.com/";
                     config.showArcGISBasemaps = true;
-                    window.addEventListener("resize", lang.hitch(this, this.resize));
-
                     this.basemapGallery = new BasemapGallery(config, "basemapGalleryDiv");
                     this.basemapGallery.startup();
-                    //  this._responsive();
-                    this.resize();
-                     this.map.on("click", lang.hitch(this, this.closeDialog));
+                    this.main.resizeTemplate();
                 },
-                closeDialog: function() {
-                    if(registry.byId("basemapDialog").open)
-                        registry.byId("basemapDialog").hide();
+                onOpen: function(){
+                    
+                },
+                onClose: function(){
+                    
                 }
+
 
 
 
